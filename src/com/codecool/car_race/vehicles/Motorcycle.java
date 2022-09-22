86e65b04fc17017eb280339a5ed565e3cc350604
Sanyi <s.truczkai@gmail.com>
2022-09-22 19:55:22 +0200

    Created subclasses: Car, Motorcycle, Truck

---
+++ src/com/codecool/car_race/vehicles/Motorcycle.java
@@ -0,0 +1,13 @@
+package com.codecool.car_race.vehicles;
+
+public class Motorcycle extends Vehicle{
+    @Override
+    public void prepareForLap() {
+
+    }
+
+    @Override
+    public void moveForAnHour() {
+
+    }
+}
---
+++ src/com/codecool/car_race/vehicles/Truck.java
@@ -0,0 +1,13 @@
+package com.codecool.car_race.vehicles;
+
+public class Truck extends Vehicle{
+    @Override
+    public void prepareForLap() {
+
+    }
+
+    @Override
+    public void moveForAnHour() {
+
+    }
+}
