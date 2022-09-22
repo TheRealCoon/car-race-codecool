7c2cc3199cb6e27c70a7d24938988ba08a2ea303
Sanyi <s.truczkai@gmail.com>
2022-09-22 19:49:30 +0200

    Implemented alpha version of Vehicle class

--- src/com/codecool/car_race/vehicles/Vehicle.java
+++ src/com/codecool/car_race/vehicles/Vehicle.java
@@ -1,4 +1,18 @@
 package com.codecool.car_race.vehicles;
 
-public class Vehicle {
+import com.codecool.car_race.Race;
+
+public abstract class Vehicle {
+    //private static final int YELLOW_FLAG_SPEED = 75;
+    private String name;
+    private final int normalSpeed;
+    private int actualSpeed;
+    private int distanceTraveled;
+
+
+    public abstract void prepareForLap() {
+        //actualSpeed = (race.isYellowFlagActive()) ? YELLOW_FLAG_SPEED : normalSpeed;
+    }
+
+    public abstract void moveForAnHour(){}
 }
