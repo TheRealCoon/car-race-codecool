cbe2c26f4d7c819f3e97df19f9bfdd8e0253e813
Sanyi <s.truczkai@gmail.com>
2022-09-22 18:40:23 +0200

    implemented alpha version of simulateRace()

--- src/com/codecool/car_race/Race.java
+++ src/com/codecool/car_race/Race.java
@@ -1,12 +1,21 @@
 package com.codecool.car_race;
 
 public class Race {
+    private final int raceTime = 50;
+    public Race() {
+    }
 
     /**
      * Simulates the passing of time by advancing the weather and
      * moving the vehicles for the duration of a whole race.
      */
     public void simulateRace() {
+        for (int i = 0; i < raceTime; i++) {
+            Weather.advance();
+            for (Vehicle vehicle: racers) {
+                vehicle.moveForAnHour();
+            }
+        }
     }
 
     /**
