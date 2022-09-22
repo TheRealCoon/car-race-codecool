c4140511d5e941900001b79e19b55b88bdfd5f4d
Sanyi <s.truczkai@gmail.com>
2022-09-22 19:22:04 +0200

    Implemented alpha version of Weather class

--- src/com/codecool/car_race/Weather.java
+++ src/com/codecool/car_race/Weather.java
@@ -1,7 +1,17 @@
 package com.codecool.car_race;
 
-public class Weather {
-    private boolean rain = false;
-    Weather(){}
+import java.util.Random;
 
+public class Weather {
+    private static final int CHANCE_OF_RAIN = 30;
+    private static boolean isRaining = false;
+
+    public static void advance() {
+        Random random = new Random();
+        isRaining = random.nextInt(0, 100) < 30;
+    }
+
+    public static boolean isRaining(){
+        return isRaining;
+    }
 }
