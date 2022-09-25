ea8043bacc14f28ef10a85e5aeea6ba62922fc35
Sanyi <s.truczkai@gmail.com>
2022-09-25 14:56:12 +0200

    Truck class can't set the race's yellowFlag

--- src/com/codecool/car_race/vehicles/Truck.java
+++ src/com/codecool/car_race/vehicles/Truck.java
@@ -33,7 +33,6 @@ public class Truck extends Vehicle {
     public void prepareForLap(Race race) {
         generateBrokenDownState();
         setActualSpeed((isBrokenDown) ? BROKEN_DOWN_SPEED : NORMAL_TRUCK_SPEED);
-        if (isBrokenDown) race.setYellowFlag(true);
     }
 
     private void generateBrokenDownState() {
